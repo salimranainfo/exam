d4521f2ab9a8c06766f99089a0af880685fd9870
Md Salim Rana <rana.salim.rana@gmail.com>
2022-01-15 10:36:46 +0600

    Added exam topics on exam.js file

---
+++ exam.js
@@ -0,0 +1,21 @@
+// Setup typescript first for below exam
+// Use typescript for all below tasks
+
+// Create 5 variables with const and let
+//  - String
+//  - Number
+//  - Boolean
+//  - Object
+//  -- Object has to contain both prperty and method
+//  - Array
+
+// Create 5 functions
+//  - Cretae 3 functions in 3 ways of writing functions
+//  - Create one return type function
+//  - Create one void type function
+
+// Cretae 2 ways of if statements
+
+// Create a switch statements
+
+// Cretae 3 ways of for loops
